38bfd1ae21d711bc253336cb668b6f65bf22d5dd
mvdream <mvdream02@gmail.com>
2019-10-01 22:11:07 +0530

    add collectstatic

--- face_recognize/settings.py
+++ face_recognize/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'faceapp'
 ]
 
@@ -120,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
