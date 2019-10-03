41dc75cac5c207c02d506b0c89921beecd9eac68
Mayur-btc <mayur.prajapati@botreetechnologies.com>
2019-10-03 15:22:14 +0530

    Ingnore csrf validation

--- faceapp/urls.py
+++ faceapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from .views import *
 from faceapp import views
+from django.views.decorators.csrf import csrf_exempt
 # from django.conf import settings
 # from django.conf.urls.static import static
 
@@ -8,6 +9,6 @@ urlpatterns = [
     # path('facedetect/',FaceRecognize.as_view(), name='detect'),
     # path('video/',views.livefe, name='detect'),
     path('video/',Home.as_view(), name='home'),
-    path('image/',FaceRecognize.as_view(), name='detect'),
+    path('image/',csrf_exempt(FaceRecognize.as_view()), name='detect'),
 
 ]
